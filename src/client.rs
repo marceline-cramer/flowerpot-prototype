44c456dfe6d070e7e3bb080d434989feb250720c
marceline-cramer <cramermarceline@gmail.com>
2023-05-25 12:13:53 -0600

    Rudimentary first-person movement

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,40 @@
+use std::f32::consts::{FRAC_PI_2, TAU};
+
 use ambient_api::prelude::*;
 
 #[main]
-pub fn main() {}
+pub fn main() {
+    let mut cursor_lock = input::CursorLockGuard::new(true);
+    let mut pitch = 0.0;
+    let mut yaw = 0.0;
+    ambient_api::messages::Frame::subscribe(move |_| {
+        let input = input::get();
+        if !cursor_lock.auto_unlock_on_escape(&input) {
+            return;
+        }
+
+        let mut direction = Vec2::ZERO;
+        let speed = 1.0; // always 1.0 because PlayerMovementInput is normalized
+        if input.keys.contains(&KeyCode::W) {
+            direction.y -= speed;
+        }
+        if input.keys.contains(&KeyCode::S) {
+            direction.y += speed;
+        }
+        if input.keys.contains(&KeyCode::A) {
+            direction.x -= speed;
+        }
+        if input.keys.contains(&KeyCode::D) {
+            direction.x += speed;
+        }
+
+        let direction = direction.normalize();
+
+        let pitch_factor = 0.01;
+        let yaw_factor = 0.01;
+        yaw = (yaw + input.mouse_delta.x * yaw_factor) % TAU;
+        pitch = (pitch + input.mouse_delta.y * pitch_factor).clamp(-FRAC_PI_2, FRAC_PI_2);
+
+        messages::PlayerMovementInput::new(direction, pitch, yaw).send_server_reliable();
+    });
+}
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::HashMap,
+    f32::consts::{FRAC_PI_2, TAU},
+    sync::Arc,
+};
 
 use ambient_api::{
     components::core::{
@@ -102,13 +106,71 @@ fn for_random_neighbors<T>(
 
 #[main]
 pub fn main() {
-    Entity::new()
-        .with_merge(make_perspective_infinite_reverse_camera())
-        .with(aspect_ratio_from_window(), EntityId::resources())
-        .with_default(main_scene())
-        .with(translation(), Vec3::new(-5.0, -5.0, 12.0))
-        .with(lookat_target(), vec3(16., 16., 0.))
-        .spawn();
+    spawn_query((player(), user_id())).bind(move |players| {
+        for (e, (_player, uid)) in players {
+            let head = Entity::new()
+                .with_merge(make_perspective_infinite_reverse_camera())
+                .with(aspect_ratio_from_window(), EntityId::resources())
+                .with_default(main_scene())
+                .with(user_id(), uid)
+                .with(translation(), Vec3::Z * 1.5)
+                .with(parent(), e)
+                .with_default(local_to_parent())
+                .with(rotation(), Quat::from_rotation_x(FRAC_PI_2))
+                .spawn();
+
+            entity::add_components(
+                e,
+                Entity::new()
+                    .with_merge(make_transformable())
+                    .with_default(cube())
+                    .with(map_position(), Vec2::new(16.0, 16.0))
+                    .with(children(), vec![head])
+                    .with(player_head_ref(), head)
+                    .with(player_pitch(), 0.0)
+                    .with(player_yaw(), 0.0),
+            );
+        }
+    });
+
+    messages::PlayerMovementInput::subscribe(move |source, msg| {
+        let Some(player_id) = source.client_entity_id() else { return; };
+
+        let direction = msg.direction.normalize_or_zero();
+        let yaw = msg.yaw % TAU;
+        let pitch = msg.pitch.clamp(-FRAC_PI_2, FRAC_PI_2);
+
+        entity::add_components(
+            player_id,
+            Entity::new()
+                .with(player_movement_direction(), direction)
+                .with(player_yaw(), yaw)
+                .with(player_pitch(), pitch),
+        );
+    });
+
+    change_query((player(), player_yaw(), player_pitch()))
+        .track_change((player_yaw(), player_pitch()))
+        .bind(move |players| {
+            for (e, (_player, yaw, pitch)) in players {
+                entity::set_component(e, rotation(), Quat::from_rotation_z(yaw));
+                if let Some(head) = entity::get_component(e, player_head_ref()) {
+                    entity::set_component(
+                        head,
+                        rotation(),
+                        Quat::from_rotation_x(FRAC_PI_2 + pitch),
+                    );
+                }
+            }
+        });
+
+    query((player(), player_movement_direction(), player_yaw())).each_frame(move |players| {
+        for (e, (_, direction, yaw)) in players {
+            let speed = 0.1;
+            let direction = Mat2::from_angle(yaw) * direction;
+            entity::mutate_component(e, map_position(), |pos| *pos += direction * speed);
+        }
+    });
 
     // spawn a ground plane
     let map_width = 32;
@@ -291,7 +353,10 @@ pub fn main() {
     {
         Entity::new()
             .with_merge(make_transformable())
-            .with(prefab_from_url(), asset::url("assets/fauna/rabbit.glb").unwrap())
+            .with(
+                prefab_from_url(),
+                asset::url("assets/fauna/rabbit.glb").unwrap(),
+            )
             .with_default(fauna())
             .with_default(bunny())
             .with(stamina(), 0.0)

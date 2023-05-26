b0dca6258600a4ca0096f885a8a39ce2316eca07
marceline-cramer <cramermarceline@gmail.com>
2023-05-26 12:16:41 -0600

    Break up server code into multiple modules

---
+++ src/crop.rs
@@ -0,0 +1,37 @@
+use ambient_api::{prelude::*, rand};
+
+use crate::{components::*, messages};
+
+/// Sets up crop-related systems.
+pub fn init_crops() {
+    // init cover crop searching
+    crate::partitioning::init_qbvh(
+        cover_crop_occupant(),
+        search_cover_crop_radius(),
+        search_cover_crop_result(),
+    );
+
+    messages::GrowTick::subscribe({
+        let growable_query = query((tile(), cover_crop_occupant())).build();
+        let mut rng = rand::thread_rng();
+        move |_, _| {
+            for (tile, (_, cover_crop)) in growable_query.evaluate() {
+                crate::map::for_random_neighbors(&mut rng, tile, |neighbor| {
+                    if entity::has_component(neighbor, cover_crop_occupant()) {
+                        None
+                    } else {
+                        entity::add_component(neighbor, cover_crop_occupant(), cover_crop);
+                        Some(())
+                    }
+                });
+            }
+        }
+    });
+
+    run_async(async move {
+        loop {
+            sleep(10.0).await;
+            messages::GrowTick::new().send_local_broadcast(true);
+        }
+    });
+}
